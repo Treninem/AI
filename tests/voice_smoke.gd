a001b36044599bf2423d4167e4b57a657dbe4f16
Treninem <trenin.98@gmail.com>
2026-08-19 00:06:57 +0300

    test(voice): add Godot voice subsystem smoke load

---
+++ tests/voice_smoke.gd
@@ -0,0 +1,20 @@
+extends SceneTree
+
+func _init() -> void:
+	var nodes: Array[Node] = [
+		AuroraVoiceBridge.new(),
+		AuroraSpeechQueue.new(),
+		AuroraEmotionController.new(),
+		AuroraAvatarVoiceController.new(),
+		AuroraWakeWordController.new(),
+		AuroraAndroidMicMonitor.new(),
+		AuroraFoxAvatarView.new(),
+	]
+	for node in nodes:
+		if node == null:
+			push_error("Voice class failed to instantiate")
+			quit(2)
+			return
+		node.free()
+	print("AURORA_VOICE_GODOT_SMOKE_OK")
+	quit(0)
