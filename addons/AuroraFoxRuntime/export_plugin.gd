be2f599b9a6d87e4e8ace1154cd3db068fc6ef77
Treninem <trenin.98@gmail.com>
2026-08-19 04:40:04 +0300

    fix: export sherpa speech runtime AAR with AuroraFox Android plugin

--- addons/AuroraFoxRuntime/export_plugin.gd
+++ addons/AuroraFoxRuntime/export_plugin.gd
@@ -14,14 +14,18 @@ func _exit_tree() -> void:
 
 class AndroidExportPlugin extends EditorExportPlugin:
 	var _plugin_name := "AuroraFoxRuntime"
+	var _sherpa_name := "sherpa-onnx-1.13.4.aar"
 
 	func _supports_platform(platform) -> bool:
 		return platform is EditorExportPlatformAndroid
 
 	func _get_android_libraries(_platform, debug) -> PackedStringArray:
-		if debug:
-			return PackedStringArray([_plugin_name + "/bin/debug/" + _plugin_name + "-debug.aar"])
-		return PackedStringArray([_plugin_name + "/bin/release/" + _plugin_name + "-release.aar"])
+		var variant := "debug" if debug else "release"
+		var prefix := _plugin_name + "/bin/" + variant + "/"
+		return PackedStringArray([
+			prefix + _plugin_name + ("-debug.aar" if debug else "-release.aar"),
+			prefix + _sherpa_name
+		])
 
 	func _get_android_dependencies(_platform, _debug) -> PackedStringArray:
 		return PackedStringArray([])
